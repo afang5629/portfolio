2cbf8ff27db9e000fb88063174bfb6a354309bdd
afang5629 <afang5629@gmail.com>
2025-01-06 00:07:59 -0500

    Updated font size

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,7 +6,7 @@ const About = () => {
       Me!
       <div className="text-[20px] py-5 w-4/5 text-left">
         <p>
-          I currently attend Carleton University. 
+          I currently attend Carleton University as a 2nd year computer science student. 
           In my free time I enjoy playing games, mainly FPS and story driven.
           Watching anime and dramas is another one of my biggest pass times.
           You can find me listening to music often, stretching genres from Jpop, Kpop, Cpop, Pop, Rap, Jazz, instrumental. <br/><br/>
--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="py-8 px-8">
+    <div className="p-8">
       <div className="text-left">
-        <p className="font-bol text-[80px] py-20">
-        Hello, <br />
-        I'm Ann
-        <br />
+        <p className="font-bol text-[50px]">
+        Welcome to my portfolio website
         </p>
-        <p>I am a second year Computer Science student streaming in game development.</p>
+        <p>I'm Ann a second year Computer Science student streaming in game development.</p>
         <p>I'm excited to begin my adventure into learning how to create fun games and stunning websites!</p>
       </div>
     </div>
